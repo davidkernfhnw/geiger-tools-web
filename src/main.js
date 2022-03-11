d2b5fe58e4ed8ebb9fd8f775ef49745acdf3b9d9
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-11 19:41:57 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <b-img alt="Geiger-logo" src="../assets/logo.png" fluid width="200px" ></b-img>
+    <div>
+  <b-card
+    title="Tool Title"
+    img-src="@/assets/tool-example.png"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 16rem;"
+    class="mx-auto"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
