295641a6877eaba911c0c3fa8bb5bfa8bc901678
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-12 14:28:43 +0100

    add example download link

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 ## Deploy on Github Pages
 See [Tutorial deploy on gh-pages](https://learnvue.co/2020/09/how-to-deploy-your-vue-app-to-github-pages/#step-1-set-publicpath-in-vue-config-js-to-our-repository-name)
 
+## How to add direct download links
+Upload File into "downloads" folder at branch gh-pages. 
+
+Example a tag
+```
+ <a href="https://davidkernfhnw.github.io/geiger-tools-web/downloads/file_name.apk" target="_blank">Download APK</a>
+```
+
 ## Project setup
 ```
 npm install
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,8 +14,10 @@
     <b-card-text>
       Some quick example text to build on the card title and make up the bulk of the card's content.
     </b-card-text>
-<!-- Todo: Not working download link -->
-    <b-button href="@/assets/apk/GEIGER-Toolbox.apk" variant="primary" >Go somewhere</b-button>
+    <b-button variant="primary" >Go somewhere</b-button>
+        <a href="https://davidkernfhnw.github.io/geiger-tools-web/downloads/GEIGER-Toolbox.apk" target="_blank" >Download</a>
+
+
   </b-card>
 </div>
   </div>
