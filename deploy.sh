ce8d3a80152840b3e823347a2a58a585517cf731
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-12 14:58:59 +0100

    update public path

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+ git push -f git@github.com:davidkernfhnw/davidkernfhnw.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
  #git push -f git@github.com:davidkernfhnw/geiger-tools-web.git main:gh-pages
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
- publicPath: '/geiger-tools-web'
+ publicPath: '/geiger-tools-web/'
 
 
 })
