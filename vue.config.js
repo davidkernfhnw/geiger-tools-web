2f8d711ab56685a220403424ad8f89da2ae87ca6
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-12 14:52:05 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/geiger-tools-web'
+ publicPath: '/geiger-tools-web'
+
+
 })
 
