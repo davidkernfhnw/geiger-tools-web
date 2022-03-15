8c7ffad578393083c4f635de74723e17df31cd06
davidkernfhnw <97611154+davidkernfhnw@users.noreply.github.com>
2022-03-15 16:38:24 +0100

    commit before deploying

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -5,6 +5,7 @@ const fs = require("fs");
   try {
     await execa("git", ["add", "."]);
     await execa("git", ["commit", "-m", "commit before deploying"]);
+    await execa("git", ["push"]);
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
